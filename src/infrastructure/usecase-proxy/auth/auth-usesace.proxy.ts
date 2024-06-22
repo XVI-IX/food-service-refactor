90ea54efed335a9cfece2461a6cca9b3271f1714
XVI-IX <oladoja14@gmail.com>
2024-06-22 16:15:46 +0100

    add auth usecase

--- src/infrastructure/usecase-proxy/auth/auth-usesace.proxy.ts
+++ src/infrastructure/usecase-proxy/auth/auth-usesace.proxy.ts
@@ -1,4 +1,4 @@
-import { Inject, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserRepository } from 'src/infrastructure/repositories/user.repository';
 import { UseCaseProxy } from '../usecase-proxy';
 import { RegisterUseCase } from 'src/usecases/auth/register.usecase';
@@ -8,6 +8,8 @@ import { JWtTokenModule } from 'src/infrastructure/services/jwt/jwt.module';
 import { ArgonModule } from 'src/infrastructure/services/argon/argon.module';
 import { RepositoriesModule } from 'src/infrastructure/repositories/repositories.module';
 import { LoginUseCase } from 'src/usecases/auth/login.usecase';
+import { ForgotPasswordUseCase } from 'src/usecases/auth/forgot_password.usecase';
+import { ResetPasswordUseCasesProxy } from 'src/usecases/auth/reset_password.usecase';
 
 export const AUTH_USECASE_CONSTANTS = {
   REGISTER: 'REGISTER_USER_USECASE_PROXY',
@@ -46,6 +48,24 @@ export class AuthUseCaseProxyModule {
             argon: ArgonService,
             userRepository: UserRepository,
           ) => new UseCaseProxy(new RegisterUseCase(userRepository)),
+        },
+        {
+          inject: [UserRepository, JwtTokenService],
+          provide: AuthUseCaseProxyModule.FORGOT_PASSWORD_USE_CASES_PROXY,
+          useFactory: (userRepository: UserRepository, jwt: JwtTokenService) =>
+            new UseCaseProxy(new ForgotPasswordUseCase(userRepository, jwt)),
+        },
+        {
+          inject: [UserRepository],
+          provide: AuthUseCaseProxyModule.RESET_PASSWORD_USE_CASES_PROXY,
+          useFactory: (
+            userRepository: UserRepository,
+            jwt: JwtTokenService,
+            argon: ArgonService,
+          ) =>
+            new UseCaseProxy(
+              new ResetPasswordUseCasesProxy(userRepository, argon, jwt),
+            ),
         },
       ],
     };
