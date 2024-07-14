4fe535b325a801e5c614ea0ea90dc2f21e8d7368
XVI-IX <oladoja14@gmail.com>
2024-07-15 00:26:34 +0100

    updaete app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,16 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './infrastructure/common/guards/auth.guard';
 import { JwtModule } from '@nestjs/jwt';
 import { JWtTokenModule } from './infrastructure/services/jwt/jwt.module';
+import { AuthUseCaseProxyModule } from './infrastructure/usecase-proxy/auth/auth-usesace.proxy';
+import { CustomerUseCaseProxyModule } from './infrastructure/usecase-proxy/customers/customers-usecase.proxy';
+import { ItemsUseCaseProxyModule } from './infrastructure/usecase-proxy/items/items-usecase.proxy';
+import { NotificationsUseCaseProxyModule } from './infrastructure/usecase-proxy/notifications/notifications-usecase.proxy';
+import { OrdersUseCaseProxyModule } from './infrastructure/usecase-proxy/orders/orders-usecase.proxy';
+import { ReviewsUseCaseProxyModule } from './infrastructure/usecase-proxy/reviews/reviews-usecase.proxy';
+import { SettingsUseCaseProxyModule } from './infrastructure/usecase-proxy/settings/settings-usecase.proxy';
+import { StoresUseCaseProxyModule } from './infrastructure/usecase-proxy/stores/stores-usecase.proxy';
+import { TimeslotUseCaseProxyModule } from './infrastructure/usecase-proxy/timeslots/timeslots-usecase.proxy';
+import { TransactionsUseCaseProxyModule } from './infrastructure/usecase-proxy/transactions/transactions-usecase.proxy';
 
 @Module({
   imports: [
@@ -23,6 +33,16 @@ import { JWtTokenModule } from './infrastructure/services/jwt/jwt.module';
     EventEmitterModule.forRoot({
       global: true,
     }),
+    AuthUseCaseProxyModule.register(),
+    CustomerUseCaseProxyModule.register(),
+    ItemsUseCaseProxyModule.register(),
+    NotificationsUseCaseProxyModule.register(),
+    OrdersUseCaseProxyModule.register(),
+    ReviewsUseCaseProxyModule.register(),
+    SettingsUseCaseProxyModule.register(),
+    StoresUseCaseProxyModule.register(),
+    TimeslotUseCaseProxyModule.register(),
+    TransactionsUseCaseProxyModule.register(),
     ControllersModule,
   ],
   controllers: [AppController],
