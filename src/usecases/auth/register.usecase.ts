afe3571cce62fa3a565d7ef7e37d91cdf11fc623
XVI-IX <oladoja14@gmail.com>
2024-07-15 23:51:36 +0100

    update usecase

--- src/usecases/auth/register.usecase.ts
+++ src/usecases/auth/register.usecase.ts
@@ -2,12 +2,17 @@ import { Logger } from '@nestjs/common';
 import { IUseCaseResponse } from 'src/domain/adapters/usecase.response';
 import { ICreateUserData } from 'src/domain/repositories/user-repository.interface';
 import { UserRepository } from 'src/infrastructure/repositories/user.repository';
+import { ArgonService } from 'src/infrastructure/services/argon/argon.service';
 
 export class RegisterUseCase {
   private logger: Logger;
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly argonService: ArgonService,
+  ) {}
 
   async register(dto: ICreateUserData): Promise<IUseCaseResponse> {
+    dto['password'] = await this.argonService.hash(dto.password);
     const user = await this.userRepository.createUser(dto);
 
     return {
